1e7ad334b00f17e614ae0eaba00b06767b22e416
benjaminheine <benjaminheine@gmx.net>
2019-10-08 23:30:34 +0200

    Some html and first tries to process data

---
+++ script.js
@@ -0,0 +1,6 @@
+var input = document.getElementById('submitbutton').addEventListener('click', SubmitClick);
+
+function SubmitClick() {
+    var submitvalue = document.getElementById.input('newtask');
+    console.log(submitval);
+}
