b050531a39fb14897d05cedc5c66663dfd850e50
benjaminheine <benjaminheine@gmx.net>
2019-10-10 23:46:23 +0200

    Got my todos listed on the page

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-// var todosubmit = document.getElementById('submitbutton').addEventListener('click', SubmitClick(event));
-// function SubmitClick(event) {
-//     var submitvalue = ('newtask');
-//     event.preventDefault()
-//     console.log(submitval);
-// }
-function SubmitClick(){
-var todosubmit = document.getElementById("newtask").value;
-safedTodo.innerHTML = "<ul>"+todosubmit+"</ul>";
-//document.writeln(todosubmit);
+var todolist = [];
+var submitvalue
+document.getElementById("submitbutton").addEventListener("click", SubmitClick);
+
+function SubmitClick(event) {
+  event.preventDefault();
+  var submitvalue = document.getElementById("newtask").value;
+  var newItem = document.createElement("LI"); // Create a <li> node
+  var textnode = document.createTextNode(submitvalue); // Create a text node
+  newItem.appendChild(textnode); // Append the text to <li>
+  var list = document.getElementById("ToDoList"); // Get the <ul> element to insert a new node
+  list.insertBefore(newItem, list.childNodes[0]); // Insert <li> before the first child of <ul>
 }
+//console.log(todolist);
+
