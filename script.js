f138743f20462e17dba614e7c61aac646652a2ca
benjaminheine <benjaminheine@gmx.net>
2019-10-13 17:07:42 +0200

    Created a checkbox and an event for the crossoff function, but can not get the value checked to change style.

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ var submitvalue
 
 document.getElementById("submitbutton").addEventListener("click", SubmitClick);
 //document.querySelector('input').addEventListener("checked", CrossToDo);
-//document.getElementById("todolist[]").addEventListener("checked", CrossToDo);
-
+//document.getElementById("submitbutton").addEventListener("click", SubmitClick);
+//document.getElementById("submitbutton").addEventListener("click", SubmitClick);
 
 
 
@@ -12,7 +12,15 @@ document.getElementById("submitbutton").addEventListener("click", SubmitClick);
 
 function CrossToDo(event){
   event.preventDefault();
+  console.log(this.checked);
   console.log(event);
+  checkbox = document.getElementById(this.id);
+  if (this.checked == true){
+    document.getElementById(this.id).style.textDecoration = 'line-through';
+  }else{
+    document.getElementById(this.id).style.textDecoration = '';
+  }
+  
 }
 
 function SubmitClick(event) {
@@ -21,26 +29,29 @@ function SubmitClick(event) {
   submitvalue = document.getElementById("newtask").value;
   // Create a <INPUT> node with attributes
   newItem = document.createElement("LI"); // Create a <li> node
-
+  var now = new Date();
+  newItem.id=now;
+  var idatt = document.createAttribute("id");
+  idatt.value = now;
   var att = document.createAttribute("style");
   att.value="text-align:center";
   newItem.setAttributeNode(att);
-  newItem.innerHTML='<input type="checkbox">' + submitvalue;
+  //newItem.innerHTML='<input type="checkbox>"' + submitvalue;
+   newItem.innerHTML="<input id="+ now + ' type="checkbox" > <label for="' + now + '"' + ">" + submitvalue + "</label>";
+ 
   
-  var now = new Date();
-  newItem.id=now;
-  var arrayid=newItem.id
+  var arrayid=newItem.id;
   todolist.push(arrayid);
 
   // create an event
-  // var event = new Event(newItem.id);
-  // document.getElementById(now).addEventListener("click", CrossToDo);
+  
 
-  //console.log(arrayid);
   
   list = document.getElementById("ToDoList"); // Get the <ul> element to insert a new node
   list.insertBefore(newItem, list.childNodes[0]); // Insert <li> before the first child of <ul>
-  
+  var checkbox = document.getElementById(newItem.id);
+  document.getElementById(now).addEventListener('change', CrossToDo);
+
 }
 
 console.log(todolist);
