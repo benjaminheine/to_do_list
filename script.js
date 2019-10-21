edb45d33496321897cd06483abe3984d0c778ca3
benjaminheine <benjaminheine@gmx.net>
2019-10-21 19:45:49 +0200

    Getting to do items after page reload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+window.onload = relaodItems;
 // Create Array of IDs from LIs
 var toDoList = [];
 // Get ID from UL for the todo items
@@ -9,7 +10,7 @@ document
   .getElementById("clearButton")
   .addEventListener("click", deleteAllItems);
 
- // Function to straight through to do item 
+// Function to straight through to do item
 function crossToDo(event) {
   event.preventDefault();
   // Only if checkbox is checked cross of the to do item otherwise let style empty
@@ -22,7 +23,7 @@ function crossToDo(event) {
   }
 }
 
-// Function to add new to do item (LI) with ID, to do value, class, button element, input element to the UL. 
+// Function to add new to do item (LI) with ID, to do value, class, button element, input element to the UL.
 // Generate an event listener for crossing and deleting of the LI.
 // Append ID to the array
 function submitClick(event) {
@@ -62,7 +63,6 @@ function submitClick(event) {
     document.forms["itemForm"].reset();
     // Store ID as key and textvalue as value in the local storage
     localStorage.setItem(now, submitValue);
-    
   }
 }
 
@@ -84,5 +84,46 @@ function deleteAllItems(e) {
     element.parentElement.removeChild(element);
     delete toDoList[i];
     localStorage.clear();
-      }
+  }
+}
+
+// Function to recreate todo items after site reload
+function relaodItems() {
+  // iterate localStorage
+  for (var i = 0; i < localStorage.length; i++) {
+    // set iteration key name
+    var key = localStorage.key(i);
+    // use key name to retrieve the corresponding value
+    var value = localStorage.getItem(key);
+    // console.log the iteration key and value
+    //console.log('Key: ' + key + ', Value: ' + value);
+    newItem = document.createElement("LI");
+    const now = key;
+    const deleteNow = now + 1;
+    newItem.id = now;
+    newItem.className = "list-group-item list-group-item-info";
+    // Create delete button
+    var itemDelete = document.createElement("button");
+    itemDelete.className = "btn btn-danger btn-sm float-right delete";
+    itemDelete.appendChild(document.createTextNode("X"));
+    itemDelete.id = now + 1;
+    newItem.appendChild(itemDelete);
+    // Create a <INPUT> node with attributes
+    const input = document.createElement("input");
+    input.type = "checkbox";
+    input.id = now;
+    newItem.appendChild(input);
+    // Add ID to the array
+    toDoList.push(newItem.id);
+    // Get to do text and put it into the LI
+    const text = document.createTextNode(value);
+    newItem.appendChild(text);
+    // Get the <ul> element to insert a new node
+    const list = document.getElementById("toDoList");
+    list.appendChild(newItem);
+    // create an event for crossing off the to do item
+    document.getElementById(now).addEventListener("change", crossToDo);
+    // create an event for the itemDelete button
+    document.getElementById(deleteNow).addEventListener("click", deleteItem);
+  }
 }
